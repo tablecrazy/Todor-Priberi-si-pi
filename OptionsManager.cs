4d77200ff8a1c61f61502a38b65814adb3121149
TodorBot49 <61000790+Todor223@users.noreply.github.com>
2022-11-16 14:48:48 +0200

    Add files via upload

---
+++ OptionsManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = 
+            {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "dr - Devide Remainder",
+            "ex - Exit"
+        };
+        public static void Exit()
+        {
+            return;
+        }
+    
+        public static void DevideRemainder(double a,double b)
+        {
+            Console.WriteLine($"{ a}%{ b}={ a%b}");
+        }
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+    }
+}
